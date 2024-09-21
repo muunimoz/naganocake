b72a2ec83676fea61a806934f3ee781eb7d341a4
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-09-21 16:01:01 +0000

    item_new

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,42 @@
+class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to adimin_items_path
+    else
+      @items = Item.all
+      render :index
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params.id)
+  end
+
+  def edit
+    @item = Item.find(params.id)
+  end
+  
+  def update
+    @item = Item.find(params.id)
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+  end
+end
