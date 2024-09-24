bde74381633ca3d50c8c09f760030c7db0ec9a9a
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-09-24 16:55:54 +0000

    admin_customer_index.show

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,4 +4,8 @@ class Public::CustomersController < ApplicationController
 
   def edit
   end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active )
+  end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,4 +6,11 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params.id)
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active)
+  end
+  
 end
