1d7e20f3398a4fec1232d84f5e46c02107058b3e
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-10-02 12:54:25 +0000

    cart_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details
+  has_one_attached :image
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validate :image_type
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
-  
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -10,22 +8,30 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  
   devise_scope :customer do
     get '/customers/sign_out' => 'devise/sessions#destroy'
   end
   
   scope module: :public do
+    root 'homes#top'
+
     get 'customers/mypage' => 'customers#show', as: 'mypage'
     # 'cart_items/destroy_all' => 'customers#show', as: 'mypage'
     resources :customers, only: [:index, :show]
     resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+    resources :items, only: [:index, :show] do
+      resources :cart_items, only: [:create, :update, :destroy]
+    end
     resources :cart_items, only: [:index, :update, :destroy, :create]
   end
   
-   namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :customers, only: [:index, :show, :edit, :update]
-  end
+   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
