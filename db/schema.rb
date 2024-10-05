e08cb392bfb2d0e27aed9bb935aaa960e30b2027
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-10-05 14:34:54 +0000

    oder_enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_02_130613) do
+ActiveRecord::Schema.define(version: 2024_10_05_141044) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -98,6 +98,18 @@ ActiveRecord::Schema.define(version: 2024_10_02_130613) do
     t.integer "price", null: false
     t.integer "genre_id", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.string "destination", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "grand_total", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
