eaed534afd9ae2c2b6085efb4353b5bb3b20e6b6
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-10-11 14:54:13 +0000

    order_datail_migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-  end
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -22,7 +13,9 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-     resources :orders, only: [:index, :show, :update]
+    resources :orders, only: [:index, :show, :update] do
+      resources :order_details, only: [:update]
+    end
   end
   
   devise_scope :customer do
@@ -31,15 +24,24 @@ Rails.application.routes.draw do
   
   scope module: :public do
     root 'homes#top'
-
     get 'customers/mypage' => 'customers#show', as: 'mypage'
-    # 'cart_items/destroy_all' => 'customers#show', as: 'mypage'
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/confirm' => 'orders#error'
+    get 'orders/thanks' => 'orders#thanks', as: 'thanks'
+
     resources :customers, only: [:index, :show]
     resources :addresses, only: [:index, :create, :edit, :update, :destroy]
     resources :items, only: [:index, :show] do
       resources :cart_items, only: [:create, :update, :destroy]
     end
-    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :cart_items, only: [:index]
+    resources :orders, only: [:new, :index, :create, :show]
   end
   
    
