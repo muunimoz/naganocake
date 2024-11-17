a28431f38a7e22c5497ea872a9865838994e4ced
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-11-17 15:51:47 +0000

    cart_item_error

--- db/migrate/20241001133535_create_cart_items.rb
+++ db/migrate/20241001133535_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       t.integer :amount, null: false
-
+      t.integer :customer_id, null: false
       t.timestamps
     end
   end
